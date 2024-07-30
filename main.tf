ec20bf84abfc558674bca55a7da827c26a018e02
pienczyk <88761512+pienczyk@users.noreply.github.com>
2024-07-30 16:32:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,8 @@
 resource "azurerm_resource_group" "rg1" {
    name = "rg-${var.project}-${var.env}-${local.location}-001"
    location = var.location
+   tags = {
+      environment = var.env
+      project = var.project
+   }
 }
